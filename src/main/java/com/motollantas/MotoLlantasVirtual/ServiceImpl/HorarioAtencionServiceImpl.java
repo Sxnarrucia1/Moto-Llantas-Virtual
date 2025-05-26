8038011ff0f654db683a2ef55aa334944649a2a9
Esteban Sanarrucia <123785378+Sxnarrucia@users.noreply.github.com>
2025-05-25 23:44:11 -0600

    Horario Atencion y CitaValidator creado

---
+++ src/main/java/com/motollantas/MotoLlantasVirtual/ServiceImpl/HorarioAtencionServiceImpl.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.motollantas.MotoLlantasVirtual.ServiceImpl;
+
+import com.motollantas.MotoLlantasVirtual.Service.HorarioAtencionService;
+import com.motollantas.MotoLlantasVirtual.dao.HorarioAtencionDao;
+import com.motollantas.MotoLlantasVirtual.domain.HorarioAtencion;
+import java.time.DayOfWeek;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+
+/**
+ *
+ * @author esteb
+ */
+public class HorarioAtencionServiceImpl implements HorarioAtencionService {
+
+    @Autowired
+    private HorarioAtencionDao horarioAtencion;
+
+    @Override
+    public Optional <HorarioAtencion> findByDia(DayOfWeek dia) {
+        return horarioAtencion.findByDia(dia);
+    }
+
+    @Override
+    public void Save(HorarioAtencion horario) {
+        horarioAtencion.save(horario);
+    }
+
+    @Override
+    public List<HorarioAtencion> getHorarios() {
+        return horarioAtencion.findAll();
+    }
+
+}
