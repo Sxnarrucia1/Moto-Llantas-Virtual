31ddeace0b759edac68e4ff2d185e2aa422b843a
Josué García <juetorgr7@gmail.com>
2025-05-30 01:20:43 -0600

    working login and encrypted passwords

--- src/main/java/com/motollantas/MotoLlantasVirtual/ServiceImpl/registerServiceImpl.java
+++ src/main/java/com/motollantas/MotoLlantasVirtual/ServiceImpl/registerServiceImpl.java
@@ -4,6 +4,7 @@ import com.motollantas.MotoLlantasVirtual.dao.registerDao;
 import com.motollantas.MotoLlantasVirtual.domain.User;
 import com.motollantas.MotoLlantasVirtual.Service.registerService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -12,12 +13,17 @@ public class registerServiceImpl implements registerService {
     @Autowired
     private registerDao registerDao;
 
+    @Autowired
+    private BCryptPasswordEncoder passwordEncoder;
+
     @Override
     public User register(User user) {
 
         if (registerDao.existsByEmail(user.getEmail())) {
             throw new RuntimeException("El correo ya está registrado.");
         }
+
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
 
         return registerDao.save(user);
     }
